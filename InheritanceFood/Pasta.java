cf812f2c4a8f4e75af8be4f0debd9bd30cda1618
The-Mathemagician <42478882+The-Mathemagician@users.noreply.github.com>
2019-01-17 09:33:32 -0600

    Add files via upload

---
+++ InheritanceFood/Pasta.java
@@ -0,0 +1,13 @@
+
+/**
+ * Write a description of class Pasta here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Pasta extends Food  {
+  public Pasta() {
+     super(false);
+  }
+}
+
